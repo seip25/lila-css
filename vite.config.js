28ab2d5231d30b19d9e119de73ae973ad9bc3839
Andres Paiva <87265390+seip25@users.noreply.github.com>
2026-04-05 18:27:11 -0300

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const folderBuild = "docs"
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: folderBuild,
-  }
 })
